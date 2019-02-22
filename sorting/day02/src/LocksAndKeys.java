b5fe82d9045228ce9484fe7c38d8314f6ba1ab37
draconian9908 <draconian9908@gmail.com>
2019-02-21 22:36:08 -0500

    Complete sorting day02

--- sorting/day02/src/LocksAndKeys.java
+++ sorting/day02/src/LocksAndKeys.java
@@ -1,16 +1,94 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class LocksAndKeys {
 
+    // Given, much thanks
     private static void swap(char[] A, int i, int j) {
         char t = A[i];
         A[i] = A[j];
         A[j] = t;
     }
+
+    // Taken from the QuickSort HW, much thanks
+    private static void shuffleArray(char[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int randIndex = ThreadLocalRandom.current().nextInt(i+1);
+            swap(array, i, randIndex);
+        }
+    }
+
+    // main, important function
     static char[][] locksAndKeys(char[] locks, char[] keys) {
-        // TODO: Return a 2d char array, where result[0] is the sorted locks, and result[1] is the sorted keys
         char[][] result = new char[2][];
+        if (locks.length == 0 || locks.length == 1) {
+            result[0] = locks;
+            result[1] = keys;
+            return result;
+        }
+        sort(keys,locks);
         result[0] = locks;
         result[1] = keys;
         return result;
+    }
+
+    // made to make the keys match the current order of the locks
+    private static void reOrder(char[] a, char[] b) {
+        HashMap<Character, Integer> mapping = new HashMap<>();
+        for (int i = 0; i < b.length; i++) {
+            mapping.put(b[i],i);
+        }
+        for (int i = 0; i < a.length; i++) {
+            int temp = mapping.get(a[i]);
+            swap(a,i,temp);
+        }
+        for (int i = 0; i < a.length; i++) {
+            int temp = mapping.get(a[i]);
+            swap(a,i,temp);
+        }
+    }
+
+    // Everything from here down is adapted from my QuickSort code
+    private static void sort(char[] checker, char[] checkee) {
+        reOrder(checker,checkee);
+        quickSort(checker, checkee, 0, checker.length-1);
+    }
+
+    public static void quickSort(char[] a, char[] b, int lo, int hi) {
+        if (a.length == 0 || a.length == 1) {
+            return;
+        }
+        if (lo < 0 || lo >= a.length || hi < 0 || hi >= a.length) {
+            return;
+        }
+        if (lo<hi) {
+            // sort the locks based on the keys
+            int p = partition(a, b, lo, hi);
+
+            // sort the keys based on the locks, using a hashmap
+            reOrder(a, b);
+
+            // recursively sort locks with updated keys
+            quickSort(a,b,lo,p-1);
+            quickSort(a,b,p+1,hi);
+        }
+    }
+
+    public static int partition(char[] checker, char[] checkee, int lo, int hi) {
+        char pivot = checker[lo];
+        int j = lo;
+        int i = hi;
+        while (!(i<=j) && i>=lo) {
+            if (checkee[i] < pivot) {
+                swap(checkee,i,j+1);
+                swap(checkee,j,j+1);
+                j++;
+                continue;
+            }
+            i--;
+        }
+        return j;
     }
 }
 
--- sorting/day02/src/TripleSum.java
+++ sorting/day02/src/TripleSum.java
@@ -1,7 +1,110 @@
+import java.util.Arrays;
+
 public class TripleSum {
 
-    static int tripleSum(int arr[], int sum) {
-        // TODO
-        return 0;
+    static int tripleSum(int[] arr, int sum) {
+        if (arr.length < 3) {
+            return 0;
+        }
+        boolean hasNeg = false;
+        int count = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length/2;i++) {
+            if (arr[i] < 0) {
+                hasNeg = true;
+            }
+            int needed = sum - arr[i];
+            for (int j = arr.length-1; j > i; j--) {
+                if (hasNeg) {
+                    int need = sum - (arr[i] + arr[j]);
+                    for (int k = j-1; k > i; k--) {
+                        if (arr[k] == need) {
+                            count++;
+                        }
+                    }
+                }
+                else if (arr[j] <= needed) {
+                    int need = sum - (arr[i] + arr[j]);
+                    for (int k = j-1; k > i; k--) {
+                        if (arr[k] == need) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            hasNeg = false;
+        }
+        System.out.println();
+        System.out.println();
+        return count;
     }
 }
+
+//if (arr.length < 3) {
+//        return 0;
+//        }
+//        int count = 0;
+//        LinkedList<LinkedList<Integer>> doubles = new LinkedList<>();
+//        for (int i = 0; i < arr.length-1; i++) {
+//        for (int j = 0; j < arr.length; j++) {
+//        if (j > i) {
+//        LinkedList<Integer> set = new LinkedList<>();
+//        set.add(arr[i]);
+//        set.add(arr[j]);
+//        doubles.add(set);
+//        }
+//        }
+//        }
+//        for (int el : arr) {
+//        for (int i = 0; i < doubles.size(); i ++) {
+//        LinkedList<Integer> d = doubles.get(i);
+//        if (el == d.getFirst() || el == d.getLast()) {
+//        continue;
+//        }
+//        int n = d.getFirst();
+//        int m = d.getLast();
+//        if (el + n + m == sum) {
+//        count++;
+//        }
+//        }
+//        }
+//        return count;
+
+
+
+
+//        HashMap<Integer, LinkedList<LinkedList<Integer>>> doubles = new HashMap<>();
+//        for (int i = 1; i < arr.length-1; i++) {
+//            for (int j = 2; j < arr.length; j++) {
+//                if (j > i) {
+//                    LinkedList<Integer> set = new LinkedList<>();
+//                    set.add(arr[i]);
+//                    set.add(arr[j]);
+//                    if (!doubles.containsKey(arr[i])) {
+//                        LinkedList<LinkedList<Integer>> sets = new LinkedList<>();
+//                        sets.add(set);
+//                        doubles.put(arr[i],sets);
+//                    }
+//                    else {
+//                        LinkedList<LinkedList<Integer>> sets = doubles.get(arr[i]);
+//                        sets.add(set);
+//                        doubles.put(arr[i],sets);
+//                    }
+//                }
+//            }
+//        }
+//        for (int el : arr) {
+//            System.out.println("el: " + el);
+//            LinkedList<LinkedList<Integer>> sets = doubles.getOrDefault(el,null);
+//            if (sets == null) {
+//                continue;
+//            }
+//            System.out.println("doubles: " + sets.toString());
+//            for (LinkedList<Integer> set : sets) {
+//                int n = set.getFirst();
+//                int m = set.getLast();
+//                if ((el + n + m) == sum) {
+//                    count++;
+//                }
+//            }
+//        }
