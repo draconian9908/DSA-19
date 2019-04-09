dab65f59aa0953f1abf8b45d7ceb5f964b4ed0a2
draconian9908 <draconian9908@gmail.com>
2019-04-08 23:53:03 -0400

    Complete graphs day03

--- graphs/day03/src/Board.java
+++ graphs/day03/src/Board.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -6,18 +7,20 @@ import java.util.List;
  */
 public class Board {
 
-    private int n;
+    public int size;
     public int[][] tiles;
 
-    //TODO
-    // Create a 2D array representing the solved board state
-    private int[][] goal = {{}};
+    // A 2D array representing the solved board state
+    private int[][] goal = {{1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 0}};
 
     /*
      * Set the global board size and tile state
      */
     public Board(int[][] b) {
-        // TODO: Your code here
+        tiles = b;
+        size = tiles.length;
     }
 
     /*
@@ -25,24 +28,93 @@ public class Board {
      (equal to 3 for 8 puzzle, 4 for 15 puzzle, 5 for 24 puzzle, etc)
      */
     private int size() {
-        // TODO: Your code here
-        return 0;
+        return size;
+    }
+
+
+    private int[] find(int[][] checker, int num) {
+        int[] found= new int[]{0,0};
+        boolean checked = false;
+        for (int i = 0; i < checker.length; i++) {
+            found[0] = i;
+            for (int j = 0; j < checker.length; j++) {
+                found[1] = j;
+                if (checker[i][j] == num) {
+                    checked = true;
+                    break;
+                }
+            }
+            if (checked) break;
+        }
+        return found;
     }
 
     /*
      * Sum of the manhattan distances between the tiles and the goal
      */
     public int manhattan() {
-        // TODO: Your code here
-        return 0;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int num = tiles[i][j];
+                if (num == 0) continue;
+                int[] g = find(goal, num);
+                count += Math.abs(i - g[0]);
+                count += Math.abs(j - g[1]);
+            }
+        }
+        return count;
+    }
+
+    /*
+     * Another cost function
+     */
+    public int placeCost() {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int num = tiles[i][j];
+                if (num == 0) continue;
+                int g = goal[i][j];
+                if (num != g) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     /*
      * Compare the current state to the goal state
      */
     public boolean isGoal() {
-        // TODO: Your code here
-        return false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (tiles[i][j] != goal[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    private int countInversions() {
+        int[] seen = new int[size*size];
+        int addLoc = 0;
+        int count = 0;
+        for (int[] row:tiles) {
+            for (int tile:row) {
+                if (tile == 0) {
+                    seen[addLoc] = 0;
+                    addLoc++;
+                    continue;
+                }
+                for (int el:seen) {
+                    if (tile < el || el == 0) count++;
+                }
+                seen[addLoc] = tile;
+                addLoc++;
+            }
+        }
+        return count;
     }
 
     /*
@@ -50,16 +122,62 @@ public class Board {
      * Research how to check this without exploring all states
      */
     public boolean solvable() {
-        // TODO: Your code here
-        return false;
+        int inversions = countInversions();
+        if (inversions % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    private void swap(Board board, int[] a, int[] b) {
+        int aNum = board.tiles[a[0]][a[1]];
+        int bNum = board.tiles[b[0]][b[1]];
+        board.tiles[a[0]][a[1]] = bNum;
+        board.tiles[b[0]][b[1]] = aNum;
     }
 
     /*
      * Return all neighboring boards in the state tree
      */
     public Iterable<Board> neighbors() {
-        // TODO: Your code here
-        return null;
+        List<Board> neighbors = new LinkedList<>();
+        int[] emptySpace = find(tiles, 0);// indices for empty space
+        // generate list of indices to lookup
+        int[][] lookup = new int[][]{{emptySpace[0], emptySpace[1]-1},//left
+                                    {emptySpace[0], emptySpace[1]+1},//right
+                                    {emptySpace[0]-1, emptySpace[1]},//above
+                                    {emptySpace[0]+1, emptySpace[1]}};//below
+        for (int[] look : lookup) {
+            if ((look[0] >= 0 && look[0] < size) && (look[1] >= 0 && look[1] < size)) {
+                Board temp = copyOf(this);
+                swap(temp, emptySpace, look);
+//                System.out.println(this.tiles);
+//                printBoard();
+                neighbors.add(temp);
+            }
+        }
+        return neighbors;
+    }
+
+    private Board copyOf(Board copied) {
+        Board newBoard = new Board(new int[size][size]);
+        for (int i = 0; i < size; i ++) {
+            newBoard.tiles[i] = copied.tiles[i].clone();
+        }
+        return newBoard;
+    }
+
+    public void printBoard() {
+        for (int[] row : this.tiles) {
+            for (int el : row) {
+                System.out.print(el);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     /*
@@ -73,12 +191,12 @@ public class Board {
         if (!(x instanceof Board)) return false;
         // Check if the same size
         Board y = (Board) x;
-        if (y.tiles.length != n || y.tiles[0].length != n) {
+        if (y.tiles.length != size || y.tiles[0].length != size) {
             return false;
         }
         // Check if the same tile configuration
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (this.tiles[i][j] != y.tiles[i][j]) {
                     return false;
                 }
@@ -89,14 +207,16 @@ public class Board {
 
     public static void main(String[] args) {
         // DEBUG - Your solution can include whatever output you find useful
-        int[][] initState = {{1, 2, 3}, {4, 0, 6}, {7, 8, 5}};
+        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}};// solvable
+//        int[][] initState = {{1, 2, 3}, {4, 0, 6}, {7, 8, 5}};// solvable
+//        int[][] initState = {{1, 0, 3}, {2, 4, 5}, {6, 7, 8}};// unsolvable
         Board board = new Board(initState);
+        Iterable<Board> it = board.neighbors();
 
         System.out.println("Size: " + board.size());
         System.out.println("Solvable: " + board.solvable());
         System.out.println("Manhattan: " + board.manhattan());
         System.out.println("Is goal: " + board.isGoal());
-        System.out.println("Neighbors:");
-        Iterable<Board> it = board.neighbors();
+//        System.out.println("Neighbors:" + it);
     }
 }
--- graphs/day03/src/Solver.java
+++ graphs/day03/src/Solver.java
@@ -4,12 +4,16 @@
  */
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solver {
 
     public int minMoves = -1;
     private State solutionState;
     private boolean solved = false;
+    private State initState;
+    private boolean solvable;
+    private PriorityQueue<State> states = new PriorityQueue<>((a,b) -> a.cost-b.cost);
 
     /**
      * State class to make the cost calculations simple
@@ -21,13 +25,16 @@ public class Solver {
         private int moves; // equal to g-cost in A*
         public int cost; // equal to f-cost in A*
         private State prev;
+        private boolean solvable;
 
         public State(Board board, int moves, State prev) {
             this.board = board;
             this.moves = moves;
             this.prev = prev;
-            // TODO
-            cost = 0;
+//            int h = board.placeCost();
+            int h = board.manhattan();
+            this.cost = moves + h;
+            this.solvable = board.solvable();
         }
 
         @Override
@@ -43,17 +50,23 @@ public class Solver {
      * Return the root state of a given state
      */
     private State root(State state) {
-        // TODO: Your code here
-        return null;
+        if (state.prev == null) {
+            return state;
+        }
+        state = root(state.prev);
+
+        return state.prev;
     }
 
     /*
      * A* Solver
      * Find a solution to the initial board using A* to generate the state tree
-     * and a identify the shortest path to the the goal state
+     * and to identify the shortest path to the the goal state
      */
     public Solver(Board initial) {
-        // TODO: Your code here
+        this.initState = new State(initial,0,null);
+        this.solvable = initState.solvable;
+        Iterable<Board> solution = solution();
     }
 
     /*
@@ -61,17 +74,36 @@ public class Solver {
      * Research how to check this without exploring all states
      */
     public boolean isSolvable() {
-        // TODO: Your code here
-        return false;
+        return solvable;
     }
 
     /*
      * Return the sequence of boards in a shortest solution, null if unsolvable
      */
     public Iterable<Board> solution() {
-        // TODO: Your code here
-        return null;
+        if (!isSolvable()) {
+            return null;
+        }
+        LinkedList<State> solutionTree = new LinkedList<>();
+        State state = initState;
+        solutionTree.add(state);
+        while (!state.board.isGoal()) {
+            Iterable<Board> neighbors = state.board.neighbors();
+            for (Board n : neighbors) {
+                State seen = find(solutionTree, n);
+                if (seen == null || seen.cost > (state.cost+1)) {
+                    states.add(new State(n, state.moves + 1, state));
+                }
+            }
+            state = states.poll();
+            solutionTree.add(state);
+        }
+        solutionState = state;
+        minMoves = state.moves;
+        solved = true;
+        return solutionTree.stream().map(s -> s.board).collect(Collectors.toList());
     }
+
 
     public State find(Iterable<State> iter, Board b) {
         for (State s : iter) {
@@ -86,11 +118,76 @@ public class Solver {
      * Debugging space
      */
     public static void main(String[] args) {
-        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+//        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+//        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}};
+        int[][] initState = {{8, 6, 7}, {2, 5, 4}, {3, 0, 1}};// insane
         Board initial = new Board(initState);
 
         Solver solver = new Solver(initial);
+        System.out.println("Min Moves: " + solver.minMoves);
     }
 
 
 }
+
+/*
+works...
+ */
+
+//    private void solvve(LinkedList<State> tree, State state) {
+//        if (state.board.isGoal()) {
+//            solutionState = state;
+//            minMoves = state.moves;
+//            solved = true;
+//            return;
+//        }
+//        Iterable<Board> neighbors = state.board.neighbors();
+//        for (Board n : neighbors) {
+//            State seen = find(tree, n);
+//            if (seen == null || seen.cost > (state.cost+1)) {
+//                states.add(new State(n, state.moves + 1, state));
+//            }
+//        }
+//        State progessTo = states.poll();
+//        tree.add(progessTo);
+//        solvve(tree, progessTo);
+//    }
+
+/*
+...does not
+ */
+
+//    private void solvve(LinkedList<State> tree, State state) {
+//        if (state.board.isGoal()) {
+//            solutionState = state;
+//            minMoves = state.moves;
+//            solved = true;
+//            return;
+//        }
+//        Iterable<Board> neighbors = state.board.neighbors();
+////        State progessTo = new State(new Board(new int[state.board.size][state.board.size]), state.moves+1, state);
+////        int minCost = 10000;
+//        for (Board n : neighbors) {
+//            State seen = find(tree, n);
+//            if (seen == null || seen.cost > (state.cost+1)) {//
+////                State stateNew = new State(n, state.moves + 1, state);
+//                states.add(new State(n, state.moves + 1, state));
+////                if (stateNew.cost <= minCost && stateNew.solvable) {//
+////                    minCost = stateNew.cost;
+////                    progessTo = stateNew;
+////                    tree.add(stateNew);
+////                    solvve(tree, stateNew);
+////                }
+//            }
+////            else {
+////                State check = new State(new Board(new int[state.board.size][state.board.size]),seen.moves, seen.prev);
+////                while (!check.board.equals(seen.board)) {
+////                    check = tree.pollLast();
+////                }
+////                progessTo = seen;
+////            }
+//        }
+//        State progessTo = states.poll();
+//        tree.add(progessTo);
+//        solvve(tree, progessTo);
+//    }
