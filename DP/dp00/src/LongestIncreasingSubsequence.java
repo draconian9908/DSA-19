70ec4f2cc8e524dbda9a8451beb09b72c218a7a6
draconian9908 <draconian9908@gmail.com>
2019-04-26 16:59:57 -0400

    Complete DP day00

--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -6,58 +6,20 @@ public class LongestIncreasingSubsequence {
     // Runtime: TODO
     // Space: TODO
     public static int LIS(int[] A) {
-        HashMap<int[], Integer> memo = new HashMap<>();
-//        int[] memo = new int[A.length];
-//        memo[0] = 1;
-        return rec(A, memo);
-    }
-
-    private static int rec(int[] a, HashMap<int[], Integer> memo) {
-//        for (int i = 1; i < a.length; i++) {
-//            for (int j = 0; j < i; j++) {
-//                if (a[j] < a[i] && memo[j] > memo[i]) {// a: element greater than prev element, memo: subproblem LIS greater than current LIS
-//                    memo[i] = memo[j] + 1;
-//                }
-//            }
-//        }
-//        return Arrays.stream(memo).max().getAsInt();
-
-//        int[] lens = new int[a.length];
-//        if (a.length == 0) return 0;
-//        else if (a.length == 1) return 1;
-//        else if (memo.containsKey(a)) return memo.get(a);
-//        else {
-//            int idx = 0;
-//            for (int i = 2; i <= a.length; i++) {
-//                if (a[a.length-1] > a[a.length-i]) {
-//                    int[] b = Arrays.copyOf(a, a.length - i);
-//                    lens[idx] = rec(b, memo);
-//                    memo.put(a, lens[idx]);
-//                    idx++;
-//                }
-//            }
-//        }
-//        return Arrays.stream(lens).max().getAsInt();
-
-        int len = 1;
-        if (a.length == 0) return 0;
-        else if (a.length == 1) return 1;
-//        else {
-//            int j = 2;
-//            while (a[a.length - 1] <= a[a.length - j] && j < a.length-1) {
-//                j++;
-//            }
-//            int[] b = Arrays.copyOf(a, a.length-j);
-//            len += rec(b, memo);
-//            memo.put(a, len);
-//        }
-        else {
-            if (a[a.length-1] > a[a.length-2]) {
-                int[] b = Arrays.copyOf(a, a.length - 1);
-                len += rec(b, memo);
-                memo.put(a, len);
+        if (A.length == 0) return 0;
+        if (A.length == 1) return 1;
+        int[] memo = new int[A.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = 1;
+        }
+        memo[0] = 1;
+        for (int idx = 1; idx < memo.length; idx++) {
+            for (int prev = 0; prev < idx; prev++) {
+                if (A[idx] > A[prev] && memo[idx] < memo[prev]+1) {
+                    memo[idx] = memo[prev] + 1;
+                }
             }
         }
-        return len;
+        return Arrays.stream(memo).max().getAsInt();
     }
 }
