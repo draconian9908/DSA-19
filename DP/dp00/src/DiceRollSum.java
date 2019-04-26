382a85d82a998ad1c6023601fac04dcb768371d6
draconian9908 <draconian9908@gmail.com>
2019-04-26 03:09:46 -0400

    Complete DP day00 Dice, LIS close

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,28 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(n)
+    // Space: O(n)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return rec(N, memo);
     }
 
+    private static int rec(int n, HashMap<Integer, Integer> memo) {
+        int sum = 0;
+        // base cases
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        //have we already solved this subproblem
+        else if (memo.containsKey(n)) return memo.get(n);
+        // Guess: how many sequences sum to six previous amounts? (six because 1-6 values per side of die)
+        else {// DP[i] = rec(i-1) + rec(i-2) + ... + rec(i-6)
+            for (int i = 1; i <= 6; i++) {
+                sum += rec(n-i, memo);// Recurse
+            }
+            memo.put(n, sum);// Memoize
+        }
+        return sum;// Solve/return answer
+    }
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,63 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class LongestIncreasingSubsequence {
 
     // Runtime: TODO
     // Space: TODO
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        HashMap<int[], Integer> memo = new HashMap<>();
+//        int[] memo = new int[A.length];
+//        memo[0] = 1;
+        return rec(A, memo);
+    }
+
+    private static int rec(int[] a, HashMap<int[], Integer> memo) {
+//        for (int i = 1; i < a.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                if (a[j] < a[i] && memo[j] > memo[i]) {// a: element greater than prev element, memo: subproblem LIS greater than current LIS
+//                    memo[i] = memo[j] + 1;
+//                }
+//            }
+//        }
+//        return Arrays.stream(memo).max().getAsInt();
+
+//        int[] lens = new int[a.length];
+//        if (a.length == 0) return 0;
+//        else if (a.length == 1) return 1;
+//        else if (memo.containsKey(a)) return memo.get(a);
+//        else {
+//            int idx = 0;
+//            for (int i = 2; i <= a.length; i++) {
+//                if (a[a.length-1] > a[a.length-i]) {
+//                    int[] b = Arrays.copyOf(a, a.length - i);
+//                    lens[idx] = rec(b, memo);
+//                    memo.put(a, lens[idx]);
+//                    idx++;
+//                }
+//            }
+//        }
+//        return Arrays.stream(lens).max().getAsInt();
+
+        int len = 1;
+        if (a.length == 0) return 0;
+        else if (a.length == 1) return 1;
+//        else {
+//            int j = 2;
+//            while (a[a.length - 1] <= a[a.length - j] && j < a.length-1) {
+//                j++;
+//            }
+//            int[] b = Arrays.copyOf(a, a.length-j);
+//            len += rec(b, memo);
+//            memo.put(a, len);
+//        }
+        else {
+            if (a[a.length-1] > a[a.length-2]) {
+                int[] b = Arrays.copyOf(a, a.length - 1);
+                len += rec(b, memo);
+                memo.put(a, len);
+            }
+        }
+        return len;
     }
 }
