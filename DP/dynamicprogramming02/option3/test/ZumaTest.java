4c4594bc65e831f49fb3c9372fc0295e801a501d
draconian9908 <draconian9908@gmail.com>
2019-05-06 18:49:14 -0400

    Complete basic test set

--- DP/dynamicprogramming02/option3/test/ZumaTest.java
+++ DP/dynamicprogramming02/option3/test/ZumaTest.java
@@ -2,45 +2,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ZumaTest {
+
     @Test
-    public void testOne() {
-        assertEquals(0,ZumaGame.zumaGame(0));
+    public void testEmptyBoard() {
+        char[] board = new char[]{};
+        char[] hand = new char[]{'g','w','r','b','y'};
+        assertEquals(0, ZumaGame.zumaGame(board, hand));
     }
 
-    //
-    // @Test
-    // public void testTwo() {
-    //     assertEquals(2,ZumaGame.zumaGame(2));
-    // }
-    //
-    // @Test
-    // public void testThree() {
-    //     // All the sequences that sum to 4:
-    //     // 4
-    //     // 3, 1
-    //     // 2, 2
-    //     // 2, 1, 1
-    //     // 1, 3
-    //     // 1, 2, 1
-    //     // 1, 1, 2
-    //     // 1, 1, 1, 1
-    //     assertEquals(8,ZumaGame.zumaGame(4));
-    // }
-    //
-    // @Test
-    // public void testFour() {
-    //     assertEquals(16,ZumaGame.zumaGame(5));
-    // }
-    //
-    // @Test
-    // public void testFive() {
-    //     assertEquals(125,ZumaGame.zumaGame(8));
-    // }
-    //
-    //
-    // @Test
-    // public void testSix() {
-    //     assertEquals(492,ZumaGame.zumaGame(10));
-    // }
+    @Test
+    public void testEmptyHand() {
+        char[] board = new char[]{'y','y'};
+        char[] hand = new char[]{};
+        assertEquals(-1, ZumaGame.zumaGame(board, hand));
+    }
 
+    @Test
+    public void testCantBeDone() {
+        char[] board = new char[]{'w','r','r','b','b','w'};
+        char[] hand = new char[]{'r','b'};
+        assertEquals(-1, ZumaGame.zumaGame(board, hand));
+    }
+
+     @Test
+     public void testAllSame() {
+        char[] board = new char[]{'g'};
+        char[] hand = new char[]{'g','g','g','g','g'};
+        assertEquals(2, ZumaGame.zumaGame(board, hand));
+     }
+
+     @Test
+     public void testNormal() {
+        char[] board = new char[]{'w','w','r','r','b','b','w','w'};
+        char[] hand = new char[]{'w','r','b','r','w'};
+        assertEquals(2, ZumaGame.zumaGame(board, hand));
+     }
+
+     @Test
+     public void testAccordion() {
+        char[] board = new char[]{'r','b','y','y','b','b','r','r','b'};
+        char[] hand = new char[]{'y','r','b','g','b'};
+        assertEquals(3, ZumaGame.zumaGame(board, hand));
+     }
 }
